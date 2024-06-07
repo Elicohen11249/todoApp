9cb14455901adb8e3181d718a0588fbb73ca07b6
elya_cohen <elico11249@gmail.com>
2024-06-07 10:19:34 -0400

    todoApp version 1

---
+++ login.js
@@ -0,0 +1,42 @@
+const title = "Welcome to our To Do App!"
+
+
+function logInBtnClicked() {
+    let inputEl = document.querySelector("#user-Name-input")
+    let userName = inputEl.value
+
+    document.querySelector("#Login-btn").innerText = "Logging In..."
+
+    setTimeout(displayApp, 1000)
+
+}
+
+function displayApp() {
+    document.querySelector(".todo-container").style.display = "block";
+    document.querySelector(".Wellcome").style.display = "none";
+
+    displayTitle()
+    displayTasks()
+}
+
+function displayTitleTillIndex(index) {
+    let subTitle = title.substring(0, index+1)
+   if(index < title.length-1) subTitle += '_';
+    document.querySelector("#disTitle").innerText = subTitle
+}
+
+
+
+function displayTitle() {
+    for (let i = 0; i < title.length; i++) {
+        setTimeout(function () { displayTitleTillIndex(i) }, i * 100)
+    }
+}
+/*    document.querySelector("#user-Name").innerText = userName
+
+document.addEventListener("DOMContentLoaded", function () {
+
+
+    document.querySelector("#Login-btn")
+        .addEventListener("click",lib)
+})*/
---
+++ removeTask.js
@@ -0,0 +1,19 @@
+
+async function taskDeleted(btnDeletedElement) {
+  let id = btnDeletedElement.parentNode.parentNode.getAttribute('data-task-id');
+  // tasks[id].deleted = true;
+
+  let confirming = confirm("Are you sure you want to delete it?");
+
+  if (confirming == true) {
+    await fetch(`http://localhost:3000/tasks/${id}`, {
+      method: 'DELETE'
+    })
+    tasks.find((task) => Number(task.id).toString() === id).deleted = true
+
+    displayTasks();
+  } else return
+}
+
+
+
